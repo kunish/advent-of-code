54779c930cf2eb2d79fb4f14f52408c0bd383dcb
kunish <kunish.butt@gmail.com>
2022-01-08 00:04:01 +0800

    chore: format with stylua

--- day1.lua
+++ day1.lua
@@ -1,40 +1,40 @@
 ---@param lines string[]
 local function part1(lines)
-	local ret = 0
+  local ret = 0
 
-	for i = 1, #lines - 1, 1 do
-		local cur = tonumber(lines[i])
-		local next = tonumber(lines[i + 1])
+  for i = 1, #lines - 1, 1 do
+    local cur = tonumber(lines[i])
+    local next = tonumber(lines[i + 1])
 
-		if next > cur then
-			ret = ret + 1
-		end
-	end
+    if next > cur then
+      ret = ret + 1
+    end
+  end
 
-	return ret
+  return ret
 end
 
 ---@param lines string[]
 local function part2(lines)
-	local ret = 0
+  local ret = 0
 
-	for i = 2, #lines - 1, 1 do
-		if not lines[i + 1] or not lines[i + 2] then
-			break
-		end
+  for i = 2, #lines - 1, 1 do
+    if not lines[i + 1] or not lines[i + 2] then
+      break
+    end
 
-		local cur = tonumber(lines[i - 1]) + tonumber(lines[i]) + tonumber(lines[i + 1])
-		local next = tonumber(lines[i]) + tonumber(lines[i + 1]) + tonumber(lines[i + 2])
+    local cur = tonumber(lines[i - 1]) + tonumber(lines[i]) + tonumber(lines[i + 1])
+    local next = tonumber(lines[i]) + tonumber(lines[i + 1]) + tonumber(lines[i + 2])
 
-		if next > cur then
-			ret = ret + 1
-		end
-	end
+    if next > cur then
+      ret = ret + 1
+    end
+  end
 
-	return ret
+  return ret
 end
 
-local lines = require("utils").read_lines_from("inputs/day1.txt")
+local lines = require('utils').read_lines_from('inputs/day1.txt')
 local answer1 = part1(lines)
 local answer2 = part2(lines)
 
--- day2.lua
+++ day2.lua
@@ -1,58 +1,58 @@
 ---@param lines string[]
 local function part1(lines)
-	local horizontal = 0
-	local depth = 0
+  local horizontal = 0
+  local depth = 0
 
-	for _, line in ipairs(lines) do
-		local splited = require("utils").split_string(line, " ")
-		local cmd = splited[1]
-		local val = tonumber(splited[2])
+  for _, line in ipairs(lines) do
+    local splited = require('utils').split_string(line, ' ')
+    local cmd = splited[1]
+    local val = tonumber(splited[2])
 
-		if cmd == "down" then
-			depth = depth + val
-		end
+    if cmd == 'down' then
+      depth = depth + val
+    end
 
-		if cmd == "up" then
-			depth = depth - val
-		end
+    if cmd == 'up' then
+      depth = depth - val
+    end
 
-		if cmd == "forward" then
-			horizontal = horizontal + val
-		end
-	end
+    if cmd == 'forward' then
+      horizontal = horizontal + val
+    end
+  end
 
-	return horizontal * depth
+  return horizontal * depth
 end
 
 ---@param lines string[]
 local function part2(lines)
-	local horizontal = 0
-	local depth = 0
-	local aim = 0
+  local horizontal = 0
+  local depth = 0
+  local aim = 0
 
-	for _, line in ipairs(lines) do
-		local splited = require("utils").split_string(line, " ")
-		local cmd = splited[1]
-		local val = tonumber(splited[2])
+  for _, line in ipairs(lines) do
+    local splited = require('utils').split_string(line, ' ')
+    local cmd = splited[1]
+    local val = tonumber(splited[2])
 
-		if cmd == "down" then
-			aim = aim + val
-		end
+    if cmd == 'down' then
+      aim = aim + val
+    end
 
-		if cmd == "up" then
-			aim = aim - val
-		end
+    if cmd == 'up' then
+      aim = aim - val
+    end
 
-		if cmd == "forward" then
-			horizontal = horizontal + val
-			depth = depth + aim * val
-		end
-	end
+    if cmd == 'forward' then
+      horizontal = horizontal + val
+      depth = depth + aim * val
+    end
+  end
 
-	return horizontal * depth
+  return horizontal * depth
 end
 
-local lines = require("utils").read_lines_from("inputs/day2.txt")
+local lines = require('utils').read_lines_from('inputs/day2.txt')
 local answer1 = part1(lines)
 local answer2 = part2(lines)
 
