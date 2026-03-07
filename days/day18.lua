3482927fa242c98e6a49de2366d6b27147ba184f
kunish <17328586+kunish@users.noreply.github.com>
2026-03-07 12:31:06 +0800

    style: format Lua day solver files

--- days/day18.lua
+++ days/day18.lua
@@ -1,222 +1,215 @@
-
-function day18_string(node) 
-	local ret = "["
-	if node.left.value == nil then 
-		ret = ret .. day18_string(node.left)
-	else 
-		ret = ret .. string.format("%d", node.left.value)
-	end
-	ret = ret .. ","
-	if node.right.value == nil  then 
-		ret = ret .. day18_string(node.right)
-	else 
-		ret = ret .. string.format("%d", node.right.value)
-	end
-	ret = ret .. "]"
-	return ret
+function day18_string(node)
+  local ret = '['
+  if node.left.value == nil then
+    ret = ret .. day18_string(node.left)
+  else
+    ret = ret .. string.format('%d', node.left.value)
+  end
+  ret = ret .. ','
+  if node.right.value == nil then
+    ret = ret .. day18_string(node.right)
+  else
+    ret = ret .. string.format('%d', node.right.value)
+  end
+  ret = ret .. ']'
+  return ret
 end
 
-function day18_magnitude(node) 
-	if node.value ~= nil then 
-		return node.value
-	end
-	return 3*day18_magnitude(node.left) + 2*day18_magnitude(node.right)
+function day18_magnitude(node)
+  if node.value ~= nil then
+    return node.value
+  end
+  return 3 * day18_magnitude(node.left) + 2 * day18_magnitude(node.right)
 end
 
-function day18_add(a,b) 
-	local node = {}
-	node.left = a
-	node.right = b 
-	node.left.parent = node
-	node.right.parent = node
-	node.value = nil
-	return node
+function day18_add(a, b)
+  local node = {}
+  node.left = a
+  node.right = b
+  node.left.parent = node
+  node.right.parent = node
+  node.value = nil
+  return node
 end
 
-function day18_explode(node, depth) 
-	if node.value ~= nil then 
-		return false 
-	end
-	if depth > 4 then 
-		-- if at leaves
-		if node.left.value ~= nil and node.right.value ~= nil then 
-			local leftval = node.left.value
-			local rightval = node.right.value
-			node.value = 0
-			node.left = nil
-			node.right = nil
-			-- move left
-			local upnode = node.parent 
-			local thisnode = node
-			while upnode ~= nil and thisnode == upnode.left do 
-				thisnode = upnode
-				upnode = thisnode.parent
-			end
-			if upnode ~= nil then 
-				local downnode = upnode.left
-				while downnode.value == nil do 
-					downnode = downnode.right
-				end 
-				downnode.value = downnode.value + leftval
-			end
-			-- move right
-			upnode = node.parent
-			thisnode = node 
-			while upnode ~= nil and thisnode == upnode.right do 
-				thisnode = upnode
-				upnode = thisnode.parent
-			end
-			if upnode ~= nil then 
-				local downnode = upnode.right
-				while downnode.value == nil do 
-					downnode = downnode.left
-				end 
-				downnode.value = downnode.value + rightval
-			end
-			return true
-		end
-	end
-	
-	local left = false
-	local right = false
-	if node.left.value == nil then 
-		left = day18_explode(node.left, depth + 1)
-	end
-	if left then 
-		return true 
-	end
-	if node.right.value == nil then 
-		right = day18_explode(node.right, depth + 1)
-	end 
-	return right
-	
-	--return false
+function day18_explode(node, depth)
+  if node.value ~= nil then
+    return false
+  end
+  if depth > 4 then
+    -- if at leaves
+    if node.left.value ~= nil and node.right.value ~= nil then
+      local leftval = node.left.value
+      local rightval = node.right.value
+      node.value = 0
+      node.left = nil
+      node.right = nil
+      -- move left
+      local upnode = node.parent
+      local thisnode = node
+      while upnode ~= nil and thisnode == upnode.left do
+        thisnode = upnode
+        upnode = thisnode.parent
+      end
+      if upnode ~= nil then
+        local downnode = upnode.left
+        while downnode.value == nil do
+          downnode = downnode.right
+        end
+        downnode.value = downnode.value + leftval
+      end
+      -- move right
+      upnode = node.parent
+      thisnode = node
+      while upnode ~= nil and thisnode == upnode.right do
+        thisnode = upnode
+        upnode = thisnode.parent
+      end
+      if upnode ~= nil then
+        local downnode = upnode.right
+        while downnode.value == nil do
+          downnode = downnode.left
+        end
+        downnode.value = downnode.value + rightval
+      end
+      return true
+    end
+  end
+
+  local left = false
+  local right = false
+  if node.left.value == nil then
+    left = day18_explode(node.left, depth + 1)
+  end
+  if left then
+    return true
+  end
+  if node.right.value == nil then
+    right = day18_explode(node.right, depth + 1)
+  end
+  return right
+
+  --return false
 end
 
-function day18_split(node) 
-	local split = false
-	if node.left.value ~= nil then
-		if  node.left.value > 9 then 
-			local leftval = node.left.value
-			node.left.value = nil
-			node.left.left = {}
-			node.left.right = {}
-			node.left.left.value = math.floor(leftval/2)
-			node.left.left.parent = node.left
-			node.left.right.value = math.ceil(leftval/2)
-			node.left.right.parent = node.left
-			return true
-		end
-	else 
-		local left = day18_split(node.left)
-		if left then 
-			return true 
-		end
-	end
-	
-	if node.right.value ~= nil then  
-		if node.right.value > 9 then 
-			local rightval = node.right.value
-			node.right.value = nil
-			node.right.left = {}
-			node.right.right = {}
-			node.right.left.value = math.floor(rightval/2)
-			node.right.left.parent = node.right
-			node.right.right.value = math.ceil(rightval/2)
-			node.right.right.parent = node.right
-			return true
-		end
-	else 
-		return day18_split(node.right)
-	end
-	
-	
+function day18_split(node)
+  local split = false
+  if node.left.value ~= nil then
+    if node.left.value > 9 then
+      local leftval = node.left.value
+      node.left.value = nil
+      node.left.left = {}
+      node.left.right = {}
+      node.left.left.value = math.floor(leftval / 2)
+      node.left.left.parent = node.left
+      node.left.right.value = math.ceil(leftval / 2)
+      node.left.right.parent = node.left
+      return true
+    end
+  else
+    local left = day18_split(node.left)
+    if left then
+      return true
+    end
+  end
+
+  if node.right.value ~= nil then
+    if node.right.value > 9 then
+      local rightval = node.right.value
+      node.right.value = nil
+      node.right.left = {}
+      node.right.right = {}
+      node.right.left.value = math.floor(rightval / 2)
+      node.right.left.parent = node.right
+      node.right.right.value = math.ceil(rightval / 2)
+      node.right.right.parent = node.right
+      return true
+    end
+  else
+    return day18_split(node.right)
+  end
 end
 
 function day18_reduce(a)
-	local explode = day18_explode(a,1)
-	if explode then 
-		return true 
-	end
-	local split = day18_split(a)
-	return split
+  local explode = day18_explode(a, 1)
+  if explode then
+    return true
+  end
+  local split = day18_split(a)
+  return split
 end
 
 function day18_line(line)
-	local _,count = line:gsub(",","")
-	if( count == 0) then
-		local node = {}
-		node.left = nil
-		node.right = nil
-		node.value = tonumber(line)
-		node.parent = nil
-		return node
-	end
-	
-	local node = {}
-	local start = 1
-	local finish = 1
-	local mid = 1
-	local depth = 0
-	while finish < string.len(line) and depth >= 0  do
-		if string.sub(line, finish, finish) == '[' then 
-			depth = depth + 1
-		elseif string.sub(line, finish, finish) == ']' then 
-			depth = depth - 1
-		elseif string.sub(line, finish,finish) == ',' and depth == 1 then 
-			mid = finish
-		elseif string.sub(line, finish,finish) == ',' then 
-		end
-		finish = finish + 1
-	end
-	local node = {}
-	node.left = day18_line(string.sub(line, start+1, mid-1))
-	node.left.parent = node
-	node.right = day18_line(string.sub(line, mid+1, finish-1))
-	node.right.parent = node 
-	node.value = nil
-	node.parent = nil
-	return node
+  local _, count = line:gsub(',', '')
+  if count == 0 then
+    local node = {}
+    node.left = nil
+    node.right = nil
+    node.value = tonumber(line)
+    node.parent = nil
+    return node
+  end
+
+  local node = {}
+  local start = 1
+  local finish = 1
+  local mid = 1
+  local depth = 0
+  while finish < string.len(line) and depth >= 0 do
+    if string.sub(line, finish, finish) == '[' then
+      depth = depth + 1
+    elseif string.sub(line, finish, finish) == ']' then
+      depth = depth - 1
+    elseif string.sub(line, finish, finish) == ',' and depth == 1 then
+      mid = finish
+    elseif string.sub(line, finish, finish) == ',' then
+    end
+    finish = finish + 1
+  end
+  local node = {}
+  node.left = day18_line(string.sub(line, start + 1, mid - 1))
+  node.left.parent = node
+  node.right = day18_line(string.sub(line, mid + 1, finish - 1))
+  node.right.parent = node
+  node.value = nil
+  node.parent = nil
+  return node
 end
 
-function day18(path) 
-	local lines = readLines(path)
-	
-	local fishsum = day18_line(lines[1])
-	for i=2,#lines do 
-		fishsum = day18_add(fishsum, day18_line(lines[i]))
-		while true do 
-			if not day18_reduce(fishsum) then 
-				break
-			end
-		end
-	end
-	
-	local part1 = day18_magnitude(fishsum)
-	print(string.format("Part 1: %d", part1))
-	
-	local part2 = nil
-	for i = 1,#lines do 	
-		for j = 1,#lines do 
-			if j ~= i then 
-				local fishpair = day18_line(lines[i])
-				fishpair = day18_add(fishpair, day18_line(lines[j]))
-				while true do 
-					if not day18_reduce(fishpair) then 
-						break
-					end
-				end
-				local magnitude = day18_magnitude(fishpair)
-				if part2 == nil or magnitude > part2 then 
-					part2 = magnitude
-				end
-			end
-		end
-	end
-	
-	print(string.format("Part 2: %d", part2))
-	
-	
-	
- end
- 
+function day18(path)
+  local lines = readLines(path)
+
+  local fishsum = day18_line(lines[1])
+  for i = 2, #lines do
+    fishsum = day18_add(fishsum, day18_line(lines[i]))
+    while true do
+      if not day18_reduce(fishsum) then
+        break
+      end
+    end
+  end
+
+  local part1 = day18_magnitude(fishsum)
+  print(string.format('Part 1: %d', part1))
+
+  local part2 = nil
+  for i = 1, #lines do
+    for j = 1, #lines do
+      if j ~= i then
+        local fishpair = day18_line(lines[i])
+        fishpair = day18_add(fishpair, day18_line(lines[j]))
+        while true do
+          if not day18_reduce(fishpair) then
+            break
+          end
+        end
+        local magnitude = day18_magnitude(fishpair)
+        if part2 == nil or magnitude > part2 then
+          part2 = magnitude
+        end
+      end
+    end
+  end
+
+  print(string.format('Part 2: %d', part2))
+end
--- days/day21.lua
+++ days/day21.lua
@@ -1,120 +1,118 @@
-
-function day21_key(p1,p1_score,p2,p2_score, won) 
-	return(string.format("%d,%d,%d,%d,%d", p1, p1_score, p2, p2_score, won))
+function day21_key(p1, p1_score, p2, p2_score, won)
+  return (string.format('%d,%d,%d,%d,%d', p1, p1_score, p2, p2_score, won))
 end
 
-function day21_mod(val, modulo) 
-	return(((val - 1) % modulo) + 1)
+function day21_mod(val, modulo)
+  return (((val - 1) % modulo) + 1)
 end
 
-function day21(path) 
-	local lines = readLines(path)
-	
-	local p1_start = tonumber(string.sub(lines[1], string.len(lines[1])))
-	local p2_start = tonumber(string.sub(lines[2], string.len(lines[2])))
-	
-	local p1 = p1_start
-	local p2 = p2_start
-	
-	local dice = 1
-	local dice_rolls = 0
-	
-	local p1_score = 0
-	local p2_score = 0
-	
-	while p1_score < 1000 and p2_score < 1000 do
-		local p1_roll = 0
-		for i = 1,3 do 
-			p1_roll = p1_roll + dice
-			dice = day21_mod(dice + 1, 100)
-		end
-		dice_rolls = dice_rolls + 3
-		p1 = day21_mod(p1 + p1_roll, 10)
-		p1_score = p1_score + p1
-		if p1_score < 1000 then 
-			local p2_roll = 0
-			for i = 1,3 do 
-				p2_roll = p2_roll + dice
-				dice = day21_mod(dice + 1, 100)
-			end
-			dice_rolls = dice_rolls + 3
-			p2 = day21_mod(p2 + p2_roll, 10)
-			p2_score = p2_score + p2
-		end
-	end
-	
-	local part1 = p1_score < p2_score and p1_score * dice_rolls or p2_score * dice_rolls
-	print(string.format("Part 1: %d", part1))
-	
-	p1 = p1_start
-	p2 = p2_start
-	
-	-- for each state in a previous round, play it forward for
-	-- all possible outcomes of both turns (3 dice 1-3 produces 
-	-- outcomes 3-9, with the number of possible distinct rolls 
-	-- producing the outcome having the distribution given 
-	-- below). Add any win states to the total for the 
-	-- appropriate player. If neither player's turn results in 
-	-- a win, propagate the state to the next turn in the 
-	-- positions table, together with a sum of all possible 
-	-- states leading to that position (the number of previous 
-	-- states producing that outcome multiplied by the number of 
-	-- possible rolls on the turn producing the outcome. Continue
-	-- until no more non-win states occur.
-	local dirac = {0,0,1,3,6,7,6,3,1}
-	local positions = {}
-	local p1_win_count = 0
-	local p2_win_count = 0
-	positions[1] = {}
-	positions[1][day21_key(p1,0,p2,0,0)] = {p1,0,p2,0,1} 
-	local new_position_count = 1
-	local round = 1
-	while true do 
-		if new_position_count < 1 then 
-			break
-		end
-		round = round + 1
-		positions[round] = {}
-		new_position_count = 0
-		for k,v in pairs(positions[round-1]) do 
-			local p1_t = v[1]
-			local p1_score_t = v[2]
-			local p2_t = v[3]
-			local p2_score_t = v[4]
-			local count_t = v[5]
-			local win = tonumber(string.sub(k, string.len(k), string.len(k)))
-			if win == 0 then 
-				for j = 3,9 do 
-					local p1_t2 = day21_mod(p1_t + j, 10)
-					local p1_score_t2 = p1_score_t + p1_t2
-					if p1_score_t2 >= 21 then 
-						p1_win_count = p1_win_count + (count_t * dirac[j]);
-					else 
-						for l = 3,9 do 
-							local p2_t2 = day21_mod(p2_t + l, 10)
-							local p2_score_t2 = p2_score_t + p2_t2
-							if p2_score_t2 >= 21 then 
-								p2_win_count = p2_win_count + count_t * dirac[j] * dirac[l]
-							else 
-								-- not a win result, propagate outcome
-								local positionkey = day21_key(p1_t2,p1_score_t2,p2_t2,p2_score_t2,0) 
-								if positions[round][positionkey] == nil then 
-									positions[round][positionkey] = {p1_t2, p1_score_t2, p2_t2, p2_score_t2, count_t * dirac[j] * dirac[l]}
-									new_position_count = new_position_count + 1
-								else 
-									positions[round][positionkey][5] = positions[round][positionkey][5] + (count_t * dirac[j] * dirac[l])
-								end
-							end
-						end
-					end 
-				end 
-			end 
-		end
-	end
-	
-	local part2 =  p1_win_count > p2_win_count and p1_win_count or p2_win_count
-	
-	print(string.format("Part 2: %d", part2))
- 
- end
- 
+function day21(path)
+  local lines = readLines(path)
+
+  local p1_start = tonumber(string.sub(lines[1], string.len(lines[1])))
+  local p2_start = tonumber(string.sub(lines[2], string.len(lines[2])))
+
+  local p1 = p1_start
+  local p2 = p2_start
+
+  local dice = 1
+  local dice_rolls = 0
+
+  local p1_score = 0
+  local p2_score = 0
+
+  while p1_score < 1000 and p2_score < 1000 do
+    local p1_roll = 0
+    for i = 1, 3 do
+      p1_roll = p1_roll + dice
+      dice = day21_mod(dice + 1, 100)
+    end
+    dice_rolls = dice_rolls + 3
+    p1 = day21_mod(p1 + p1_roll, 10)
+    p1_score = p1_score + p1
+    if p1_score < 1000 then
+      local p2_roll = 0
+      for i = 1, 3 do
+        p2_roll = p2_roll + dice
+        dice = day21_mod(dice + 1, 100)
+      end
+      dice_rolls = dice_rolls + 3
+      p2 = day21_mod(p2 + p2_roll, 10)
+      p2_score = p2_score + p2
+    end
+  end
+
+  local part1 = p1_score < p2_score and p1_score * dice_rolls or p2_score * dice_rolls
+  print(string.format('Part 1: %d', part1))
+
+  p1 = p1_start
+  p2 = p2_start
+
+  -- for each state in a previous round, play it forward for
+  -- all possible outcomes of both turns (3 dice 1-3 produces
+  -- outcomes 3-9, with the number of possible distinct rolls
+  -- producing the outcome having the distribution given
+  -- below). Add any win states to the total for the
+  -- appropriate player. If neither player's turn results in
+  -- a win, propagate the state to the next turn in the
+  -- positions table, together with a sum of all possible
+  -- states leading to that position (the number of previous
+  -- states producing that outcome multiplied by the number of
+  -- possible rolls on the turn producing the outcome. Continue
+  -- until no more non-win states occur.
+  local dirac = { 0, 0, 1, 3, 6, 7, 6, 3, 1 }
+  local positions = {}
+  local p1_win_count = 0
+  local p2_win_count = 0
+  positions[1] = {}
+  positions[1][day21_key(p1, 0, p2, 0, 0)] = { p1, 0, p2, 0, 1 }
+  local new_position_count = 1
+  local round = 1
+  while true do
+    if new_position_count < 1 then
+      break
+    end
+    round = round + 1
+    positions[round] = {}
+    new_position_count = 0
+    for k, v in pairs(positions[round - 1]) do
+      local p1_t = v[1]
+      local p1_score_t = v[2]
+      local p2_t = v[3]
+      local p2_score_t = v[4]
+      local count_t = v[5]
+      local win = tonumber(string.sub(k, string.len(k), string.len(k)))
+      if win == 0 then
+        for j = 3, 9 do
+          local p1_t2 = day21_mod(p1_t + j, 10)
+          local p1_score_t2 = p1_score_t + p1_t2
+          if p1_score_t2 >= 21 then
+            p1_win_count = p1_win_count + (count_t * dirac[j])
+          else
+            for l = 3, 9 do
+              local p2_t2 = day21_mod(p2_t + l, 10)
+              local p2_score_t2 = p2_score_t + p2_t2
+              if p2_score_t2 >= 21 then
+                p2_win_count = p2_win_count + count_t * dirac[j] * dirac[l]
+              else
+                -- not a win result, propagate outcome
+                local positionkey = day21_key(p1_t2, p1_score_t2, p2_t2, p2_score_t2, 0)
+                if positions[round][positionkey] == nil then
+                  positions[round][positionkey] =
+                    { p1_t2, p1_score_t2, p2_t2, p2_score_t2, count_t * dirac[j] * dirac[l] }
+                  new_position_count = new_position_count + 1
+                else
+                  positions[round][positionkey][5] = positions[round][positionkey][5] + (count_t * dirac[j] * dirac[l])
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  local part2 = p1_win_count > p2_win_count and p1_win_count or p2_win_count
+
+  print(string.format('Part 2: %d', part2))
+end
